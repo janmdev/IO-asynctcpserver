1a09e6f7bdbffebdfb8cddeee6ef5201926b2fa1
Jan Majewski <jan.majewski@student.put.poznan.pl>
2020-11-01 11:18:55 +0100

    Add project files.

---
+++ asynctcpserver/AsyncTcpServer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asynctcpserver
+{
+    public class AsyncTcpServer : TcpServer
+    {
+        public AsyncTcpServer(IPAddress ipAddres, int port, int bufferSize) : base(ipAddres, port, bufferSize) { }
+
+        public override void RunServer()
+        {
+            listener.Start();
+            client = listener.AcceptTcpClient();
+        }
+    }
+}
---
+++ asynctcpserver/TcpServer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asynctcpserver
+{
+    public abstract class TcpServer
+    {
+        protected TcpListener listener;
+        protected TcpClient client;
+        protected byte[] buffer;
+
+        protected TcpServer(IPAddress iPAddress, int port, int bufferSize)
+        {
+            listener = new TcpListener(iPAddress, port);
+            buffer = new byte[bufferSize];
+        }
+
+        public abstract void RunServer();
+        public void StopServer()
+        {
+            try
+            {
+                client.Close();
+            }
+            catch(NullReferenceException )
+            {}
+        }
+
+        protected int Silnia(int n)
+        {
+            int suma = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                suma *= i;
+            }
+            return suma;
+        }
+    }
+}
